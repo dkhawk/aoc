ff4c070348c4481e07d8bac94b85857c1a6915bb
dkhawk <107309+dkhawk@users.noreply.github.com>
2025-09-03 04:45:53 -0600

    AOC 2016 - day 3

--- app/src/main/kotlin/com/sphericalchickens/aoc2016/day03/Day03.kt
+++ app/src/main/kotlin/com/sphericalchickens/aoc2016/day03/Day03.kt
@@ -1,6 +1,8 @@
 package com.sphericalchickens.aoc2016.day03
 
+import com.sphericalchickens.utils.println
 import com.sphericalchickens.utils.readInputLines
+import kotlin.text.trim
 
 fun main() {
     println("--- Advent of Code 2016, Day 3 ---")
@@ -19,12 +21,38 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
-    return input.size
+    return input.filterNot { it.isBlank() }.count {
+        it.isTriangle()
+    }
 }
 
 fun part2(input: List<String>): Int {
-    return input.size
+    val values = input.map { line -> line.trim().split(Regex("""\s+""")).map { it.toInt() }}.flatten()
+
+    val pivoted = buildList {
+        for (i in values.indices) {
+            if (i % 3 == 0) add(values[i])
+        }
+        for (i in values.indices) {
+            if (i % 3 == 1) add(values[i])
+        }
+        for (i in values.indices) {
+            if (i % 3 == 2) add(values[i])
+        }
+    }
+
+    return pivoted.windowed(3, 3).count { isTriangle(it) }
 }
+
+private fun String.isTriangle() : Boolean {
+    val sides = trim().split(Regex("""\s+""")).map { it.toInt() }
+
+    return isTriangle(sides)
+}
+
+private fun isTriangle(sides: List<Int>): Boolean = (sides[0] + sides[1] > sides[2]) &&
+        (sides[1] + sides[2] > sides[0]) &&
+        (sides[0] + sides[2] > sides[1])
 
 private fun runTests() {
     // Add tests here

f13b03b397627935c2c7b9aa4fd56e6318f9d71c
Dale Hawkins <dkhawk@users.noreply.github.com>
2025-12-03 04:38:58 -0700

    aoc2025 - day 3

--- app/src/main/kotlin/com/sphericalchickens/aoc2025/day03/Day03.kt
+++ app/src/main/kotlin/com/sphericalchickens/aoc2025/day03/Day03.kt
@@ -8,9 +8,9 @@ fun main() {
     // --- Development Workflow Control Panel ---
     // Set these flags to control which parts of the solution to run.
     val runPart1Tests = true
-    val runPart1Solution = false
-    val runPart2Tests = false
-    val runPart2Solution = false
+    val runPart1Solution = true
+    val runPart2Tests = true
+    val runPart2Solution = true
     // ----------------------------------------
 
     println("--- Advent of Code 2025, Day 3 ---")
@@ -50,22 +50,64 @@ fun main() {
 
 private fun runPart1Tests() {
     val testInput = """
-        
+        987654321111111
+        811111111111119
+        234234234234278
+        818181911112111
     """.trimIndent().lines()
-    check("Part 1 Test Case 1", -1, part1(testInput))
+
+    val expected = listOf(98, 89, 78, 92,)
+
+    testInput.zip(expected).forEach { (line, expected) ->
+        val a = maxJoltage(line)
+        check(line, expected, a)
+    }
+
+    check("Part 1 Test Case 1", 357, part1(testInput))
+}
+
+private fun maxJoltage(line: String): Int {
+    val max = line.dropLast(1).max()
+    val max2 = line.substringAfter(max).max()
+
+    val a = ((max - '0') * 10) + (max2 - '0')
+    return a
+}
+
+private fun maxJoltage12(line: String): Long {
+    var justAfterPrevious = 0
+
+    val digits = (11 downTo 0).map { i ->
+        val max = line.drop(justAfterPrevious).dropLast(i).max()
+        justAfterPrevious += line.drop(justAfterPrevious).indexOf(max) + 1
+        max
+    }
+
+    return digits.joinToString("").toLong()
 }
 
 private fun runPart2Tests() {
     val testInput = """
-        
+        987654321111111
+        811111111111119
+        234234234234278
+        818181911112111
     """.trimIndent().lines()
-    check("Part 2 Test Case 1", -1, part2(testInput))
+
+    val expected = listOf(987654321111, 811111111119, 434234234278, 888911112111)
+
+    testInput.zip(expected).forEach { (line, expected) ->
+        val a = maxJoltage12(line)
+        check(line, expected, a)
+    }
+
+    check("Part 2 Test Case 1", 3121910778619, part2(testInput))
 }
 
 private fun part1(input: List<String>): Int {
-    return -1
+    return input.sumOf { maxJoltage(it) }
 }
 
-private fun part2(input: List<String>): Int {
-    return -1
+private fun part2(input: List<String>): Long {
+    return input.sumOf { maxJoltage12(it) }
 }
